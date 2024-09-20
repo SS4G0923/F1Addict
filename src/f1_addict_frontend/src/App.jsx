60e047b12ec8f99e2ee9ed993db88146e5099542
Zheng Lian <ss4g0923@gmail.com>
2024-09-20 23:07:08 +0800

    Update: added dashboard page

--- src/f1_addict_frontend/src/App.jsx
+++ src/f1_addict_frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Teams from './teams';
 import F1TV from './F1TV';
 import Login from './login';
 import Register from './register';
+import Dashboard from './dashboard';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -12,12 +13,14 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/drivers" element={<Drivers />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/f1tv" element={<F1TV />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   )
