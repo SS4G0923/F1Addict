7a26260b9b3aa0c292d27345b4226530e8115359
Zheng Lian <ss4g0923@gmail.com>
2024-09-22 22:13:52 +0800

    Update: added side bar on dashboard page

--- src/f1_addict_frontend/src/dashboard.jsx
+++ src/f1_addict_frontend/src/dashboard.jsx
@@ -1,7 +1,20 @@
-export default function Dashboard(){
+import DashboardBanner from './components/dashboardBanner';
+import { Link } from 'react-router-dom';
+import './dashboard.css';
+
+export default function Dashboard() {
     return(
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <>
+            <DashboardBanner/>
+            <div className="dashboardWrapper">
+                <div className="sidebar">
+                    <ul>
+                        <li><Link to="/teams" style={{textDecoration: 'none', color: 'white'}}>Teams</Link></li>
+                        <li><Link to="/drivers" style={{textDecoration: 'none', color: 'white'}}>Drivers</Link></li>
+                        <li><Link to="/articles" style={{textDecoration: 'none', color: 'white'}}>Articles</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
