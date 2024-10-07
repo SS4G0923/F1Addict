b66eabb13d5270dfcdf2ca51b8df2b5450bc11ef
Zheng Lian <ss4g0923@gmail.com>
2024-10-07 22:09:02 +0800

    Update: added table field annotation for schedule model

--- src/main/java/com/f1addict/f1addictbackend/Entity/Schedule.java
+++ src/main/java/com/f1addict/f1addictbackend/Entity/Schedule.java
@@ -1,5 +1,6 @@
 package com.f1addict.f1addictbackend.Entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 @Data
@@ -7,6 +8,7 @@ public class Schedule {
     String raceName;
     String raceCountry;
     String date;
+    @TableField(exist = false)
     String description;
     int raceNum;
 }
