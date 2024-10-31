b67391ec194c1d78fb6c530c040c7b69c00f23ba
Zheng Lian <ss4g0923@gmail.com>
2024-10-31 22:16:31 +0800

    Update: added router for team dashboard page

--- src/f1_addict_frontend/src/App.jsx
+++ src/f1_addict_frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './dashboard';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardDrivers from './dashboardDrivers';
+import DashboardTeams from './dashboardTeams';
 
 function App() {
   
@@ -23,6 +24,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/drivers" element={<DashboardDrivers />} />
+        <Route path="/dashboard/teams" element={<DashboardTeams />} />
       </Routes>
     </Router>
   )
