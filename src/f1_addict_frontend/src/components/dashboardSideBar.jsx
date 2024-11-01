fbb46b08fb7ecfe4bfee6009c0bdb25f16cfc96e
Zheng Lian <ss4g0923@gmail.com>
2024-11-01 14:18:30 +0800

    Update: added dashboard side bar component to reduce code redundency

--- src/f1_addict_frontend/src/components/dashboardSideBar.jsx
+++ src/f1_addict_frontend/src/components/dashboardSideBar.jsx
@@ -1,7 +1,14 @@
-export default function DashboardSideBar() {
-    return (
+import { Link } from 'react-router-dom';
+import './dashboardSidebar.css';
+
+export default function DashboardSidebar(){
+    return(
         <div className="sidebar">
-            <h1>Dashboard</h1>
+            <ul>
+                <li><Link to="teams" style={{textDecoration: 'none', color: 'white'}}>Teams</Link></li>
+                <li><Link to="drivers" style={{textDecoration: 'none', color: 'white'}}>Drivers</Link></li>
+                <li><Link to="articles" style={{textDecoration: 'none', color: 'white'}}>Articles</Link></li>
+            </ul>
         </div>
-    );
+    )
 }
